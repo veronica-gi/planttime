ba471cc4ef4dada626732cf21dfc84fc6de95ae0
Verónica García <veronicagaiglesias@gmail.com>
2025-11-10 16:18:01 +0100

    feat: corrección de rutas

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import App from './app/App.tsx'
+import './ui/styles/app.css'
+import './ui/styles/tailwind.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
   </StrictMode>,
 )
+
