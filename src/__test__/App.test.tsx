71578f5dc3d770ecfed6e8bd4c64c1cec49ce5da
Verónica García <veronicagaiglesias@gmail.com>
2026-01-02 16:05:19 +0100

    chore: add initial test setup for App component

--- src/__test__/App.test.tsx
+++ src/__test__/App.test.tsx
@@ -1,3 +1,12 @@
+import { render, screen } from '@testing-library/react';
+import App from '../app/App';
+
+describe('App', () => {
+  test('renders PlantTime title', () => {
+    render(<App />);
+    expect(screen.getByText(/planttime/i)).toBeInTheDocument();
+  });
+});
 
 
 
