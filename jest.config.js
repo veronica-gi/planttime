0a8381420349e6312e73aa4af237a97225277327
Verónica García <veronicagaiglesias@gmail.com>
2026-01-02 13:38:16 +0100

    chore: configure Jest with ts-jest for TypeScript testing

---
+++ jest.config.js
@@ -0,0 +1,7 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  testMatch: ['**/__tests__/**/*.test.ts?(x)'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+};
