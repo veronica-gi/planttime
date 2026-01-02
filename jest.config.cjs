f79d51301c9463ec3ac00356b30ad3ecfa35f947
Verónica García <veronicagaiglesias@gmail.com>
2026-01-02 16:32:28 +0100

    test: setup Jest and add basic App render test

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,8 +1,12 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
   preset: 'ts-jest',
-  testEnvironment: 'jsdom',
+  testEnvironment: 'jest-environment-jsdom',
   testMatch: ['**/__tests__/**/*.test.ts?(x)'],
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
   setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
+
+  transform: {
+    '^.+\\.(ts|tsx)$': ['ts-jest', { tsconfig: 'tsconfig.jest.json' }],
+  },
 };
--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -4,7 +4,10 @@ import App from '../app/App';
 describe('App', () => {
   test('renders PlantTime title', () => {
     render(<App />);
-    expect(screen.getByText(/planttime/i)).toBeInTheDocument();
+    expect(
+  screen.getByText((content) => content.includes("PlanTime"))
+).toBeInTheDocument();
+
   });
 });
 
